6e8f210200491882fc0055bf82d51d18408e7b2c
Brittani Ericksen <heybrittani@gmail.com>
2020-07-28 17:07:28 -0400

    Added new python practice files

---
+++ hello2.py
@@ -0,0 +1,5 @@
+user_name = input("WHAT IS YOUR NAME? ")
+name_length = len(user_name)
+print("HELLO " + user_name.upper())
+print("YOUR NAME HAS %d LETTERS IN IT!" % name_length)
+
---
+++ hello_world.py
@@ -0,0 +1,5 @@
+def self_introduction():
+    print("Hi! My name is Brittani Ericksen.")
+
+self_introduction()
+
